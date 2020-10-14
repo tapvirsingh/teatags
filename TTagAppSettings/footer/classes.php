c77cefeb0f46a0443a38222301520ff9509e7d0b
Tapvir Singh <tapvir@gmail.com>
2020-10-14 20:38:56 +0530

    Added social-link-class in classes.php

--- TTagAppSettings/footer/classes.php
+++ TTagAppSettings/footer/classes.php
@@ -7,6 +7,7 @@ return [
 			'footerLinks'=>'ttag-footer-links col-sm-6',
 			'footerHLink'=>'ttag-footer-h-link d-inline-flex m-3',
 			'footerSubCap'=>'ttag-footer-sublink-caption',
+			'social-link-class' => 'm-3 text-light d-inline-flex ttag-social-link',
 			// Bootstrap or custom css classes.
 			'class' => 'p-5  bg-dark'
 	];
