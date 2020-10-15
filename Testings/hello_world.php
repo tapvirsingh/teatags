f4a130846cf713abcf537322fb4499e90138ccd4
Tapvir Singh <tapvir@gmail.com>
2020-10-16 04:20:29 +0530

    deleted extra files.

--- Testings/hello_world.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Include the global TTag's configuration file.
-require_once('../TTagConfig.php');
-
-// Use HtmlTTag
-use Src\ContainerTags\ExtendedContainers\{HtmlTTag};
-
-// Displays Hello World!.
-New HtmlTTag('Hello World!');
