d3fbd1e3af1e581d3c617f9c66a4a57679f0f1a0
Tapvir Singh <tapvir@gmail.com>
2020-10-23 20:05:00 +0530

    Started with AccordionTTag

---
+++ Src/TTags/AccordionTTag.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Description of AccordionTTag
+ *
+ * @author Tapvir Singh.
+ */
+namespace Src\TTags;
+
+use Src\ContainerTags\TapvirTagContainer;
+
+class AccordionTTag extends TapvirTagContainer{
+
+};
