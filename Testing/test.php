c59fb89a8b9e5ef15af3545000660dd1b9b0d9ee
Tapvir Singh <tapvir@gmail.com>
2020-11-03 22:33:16 +0530

    Removed zipped and test files

--- Testing/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Include the global TTag's configuration file.
-require_once('../ttag-config.php');
-
-// Use HtmlTTag.
-use Src\TTags\{HtmlTTag};
-
-// Displays The World Loves Tea!.
-new HtmlTTag('The World Loves Tea!');
