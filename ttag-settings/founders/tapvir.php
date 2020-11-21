0ab6b552db7d0235aab88dc50ec2dd956d33821b
Tapvir Singh <tapvir@gmail.com>
2020-11-21 23:06:02 +0530

    Upgrading founders

---
+++ ttag-settings/founders/tapvir.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+		'name' => 'Tapvir Singh',
+
+		'bio' => '',
+		// Display picture
+		'dp' => [
+				// Name of the picture file.
+				// 'file'=> ttag_RootImage('DSC_7924.JPG'),
+				'file'=> ROOT.'/Images/DSC_7924.JPG',
+				// https://getbootstrap.com/docs/4.5/utilities/borders/#sizes
+				'style' => 'rounded-circle', 
+				// width and height
+				'size' => '64', 
+			],
+		// social links
+		'social' => [
+			'twitter' => 'https://twitter.com/tapvir_singh',
+			'facebook' => 'https://www.facebook.com/tapvir/',
+			'linkedin' => 'https://www.linkedin.com/in/tapvir/',
+			'youtube'=> 'https://www.youtube.com/user/tapvir/',
+			'github' => 'https://github.com/tapvirsingh',
+		],
+
+		'website'=> 'https://tapvir.blogspot.com/',
+		'website-name'=> 'tapvir.blogspot.com',
+		'text' => 'Founder ',
+
+		'extra-row-class' => 'text-center text-white mt-2',
+		'social-link-class' => 'm-2 text-light d-inline-flex ttag-social-link',
+
+	];
